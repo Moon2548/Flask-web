f0bd09e77a492eb70f95219fdceb2b8f0fb320cc
Taveeporn Chuaybumrung <taweeporn16078@gmail.com>
2025-02-09 17:59:50 +0700

    create noteapp.py

---
+++ noteapp.py
@@ -0,0 +1,24 @@
+import flask
+import models
+import forms
+
+app = flask.Flask(__name__)
+app.config["SECRET_KEY"] = "This is secret key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+models.init_app(app)
+
+
+@app.route("/")
+def index():
+    db = models.db
+    notes = db.session.execute(
+        db.select(models.Note).order_by(models.Note.title)
+    ).scalars()
+    return flask.render_template(
+        "index.html",
+        notes=notes,
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
