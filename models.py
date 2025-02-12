8a9755e6bb276bee906d06562176d011ff844acc
Taveeporn Chuaybumrung <taweeporn16078@gmail.com>
2025-02-12 22:36:43 +0700

    add password_hash in models.py

--- models.py
+++ models.py
@@ -81,3 +81,22 @@ class User(db.Model, UserMixin):
     created_date = mapped_column(sa.DateTime(timezone=True), server_default=func.now())
     updated_date = mapped_column(sa.DateTime(timezone=True), server_default=func.now())
     roles: Mapped[list[Role]] = relationship(secondary=user_roles)
+
+    @hybrid_property
+    # this will prevent our password_hash from being returned in a request
+    # to users
+
+    def password_hash(self):
+        raise Exception("Password hashes may not be viewed.")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
+        self._password_hash = password_hash.decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
+
+    # this prevents that Exception being raised everytime we try to call the
+    # .to_dict() method in a request that returns information from users
+    serialize_rules = ("-_password_hash",)
